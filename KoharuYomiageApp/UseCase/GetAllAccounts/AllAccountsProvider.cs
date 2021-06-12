f76fffb14f3fbfc042769eb2f83c3ad1e8c8cabc
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2021-06-12 14:18:45 +0900

    Update AllAccountsProvider to return both mastodon and misskey account

--- KoharuYomiageApp/UseCase/GetAllAccounts/AllAccountsProvider.cs
+++ KoharuYomiageApp/UseCase/GetAllAccounts/AllAccountsProvider.cs
@@ -10,18 +10,25 @@ namespace KoharuYomiageApp.UseCase.GetAllAccounts
     public class AllAccountsProvider : IGetAllAccounts
     {
         readonly IMastodonAccountRepository _mastodonAccountRepository;
+        readonly IMisskeyAccountRepository _misskeyAccountRepository;
 
-        public AllAccountsProvider(IMastodonAccountRepository mastodonAccountRepository)
+        public AllAccountsProvider(IMastodonAccountRepository mastodonAccountRepository,
+            IMisskeyAccountRepository misskeyAccountRepository)
         {
             _mastodonAccountRepository = mastodonAccountRepository;
+            _misskeyAccountRepository = misskeyAccountRepository;
         }
 
         public async Task<IEnumerable<AccountData>> GetAllAccounts(CancellationToken cancellationToken)
         {
-            var data = await _mastodonAccountRepository.GetAllMastodonAccounts(cancellationToken);
-            return data.Select(d =>
+            var mastodonData = await _mastodonAccountRepository.GetAllMastodonAccounts(cancellationToken);
+            var misskeyData = await _misskeyAccountRepository.GetAllMisskeyAccounts(cancellationToken);
+            return mastodonData.Select(d =>
                     new AccountData(d.AccountIdentifier.Value, d.Username.Value, d.Instance.Value, d.IconUrl.IconUrl,
                         d.IsReadingPostsFromThisAccount.Value))
+                .Concat(misskeyData.Select(d =>
+                    new AccountData(d.AccountIdentifier.Value, d.Username.Value, d.Instance.Value, d.IconUrl.IconUrl,
+                        d.IsReadingPostsFromThisAccount.Value)))
                 .ToList();
         }
     }
